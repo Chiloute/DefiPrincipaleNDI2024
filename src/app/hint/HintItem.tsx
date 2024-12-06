7d3f681e8cf4429be8b1b9decd9face1c2625262
jarrousse <jarrousse@et.esiea.fr>
2024-12-06 07:43:38 +0100

    rendu final

--- src/app/hint/HintItem.tsx
+++ src/app/hint/HintItem.tsx
@@ -2,16 +2,18 @@
 
 import React, { useState } from 'react';
 import './HintItem.css';
+import { useStore } from '../page';
 
 interface HintProps {
   x: number; // La position (par ex., 1, 4, etc.)
   y: number; // La position (par ex., 1, 4, etc.)
+  generation: number; 
   intensity?: number; // l'intensité (1:3)
   title?: string;    // Le contenu du point
   content: string;  // Le texte de l'aide
 }
 
-const HintItem: React.FC<HintProps> = ({ x, y, intensity=1, title="", content }) => {
+const HintItem: React.FC<HintProps> = ({ x, y, generation, intensity=1, title="", content }) => {
   var color = "";
   switch (intensity) {
     case 2:
@@ -24,11 +26,16 @@ const HintItem: React.FC<HintProps> = ({ x, y, intensity=1, title="", content })
       color = "green";
       break;
   }
-  return (
-    <button className='labels' style={{ position: 'absolute', left: x, top: y, '--bg-color': color}}>
-      {content}
-    </button>
-  );
+  const temperature = useStore((state) => state.temperature);
+
+  if (generation == temperature)
+    return (
+      <button className='labels' style={{ position: 'absolute', left: `${x}%`, top: `${y}%`, '--bg-color': color}}>
+        {content}
+      </button>
+    );
+  else
+    return (<button className='labels' style={{display: 'none'}}></button>);
 };
 
 export default HintItem;
--- src/app/hint/page.tsx
+++ src/app/hint/page.tsx
@@ -4,27 +4,230 @@ import React from 'react';
 import HintItem from './HintItem';
 import './HintPage.css';
 import Image from 'next/image'
-import { useStore } from '../page';
 
 const HintPage = () => {
   
-  const temperature = useStore((state) => state.temperature);
   return (
     <div className='HintTable'>
         <div className='left split'>
           <Image src="/terre.png" alt="une personne" width="400" height="400" />
           <HintItem
-            x={10}
+            x={35}
+            y={18}
+            generation={1}
+            content="Blanchissement des coraux"
+          />
+          <HintItem
+            x={55}
+            y={25}
+            generation={1}
+            content="Expansion des zones mortes"
+          />
+          <HintItem
+            x={48}
+            y={30}
+            generation={1}
+            content="Perturbation des cycles de reproduction"
+          />
+          <HintItem
+            x={42}
+            y={4}
+            generation={1}
+            content="Fonte accrue des glaciers et calottes polaires"
+          />
+          <HintItem
+            x={35}
+            y={18}
+            generation={2}
+            intensity={2}
+            content="Blanchissement massif des coraux"
+          />
+          <HintItem
+            x={55}
+            y={25}
+            generation={2}
+            content="Migration des espèces"
+          />
+          <HintItem
+            x={48}
+            y={40}
+            generation={2}
+            content="Effondrement de certaines pêcheries"
+          />
+          <HintItem
+            x={42}
+            y={4}
+            generation={2}
+            intensity={2}
+            content="Accélération de l'élévation du niveau de la mer"
+          />
+          <HintItem
+            x={35}
+            y={18}
+            generation={3}
+            intensity={3}
+            content="Disparition des récifs coralliens"
+          />
+          <HintItem
+            x={45}
+            y={28}
+            generation={3}
+            intensity={2}
+            content="Modification majeure des courants océaniques"
+          />
+          <HintItem
+            x={48}
+            y={40}
+            generation={3}
+            intensity={2}
+            content="Extinction d'espèces sensibles"
+          />
+          <HintItem
+            x={45}
             y={10}
-            content=""
+            generation={3}
+            intensity={2}
+            content="Réduction massive de l'oxygène océanique"
+          />
+          <HintItem
+            x={35}
+            y={18}
+            generation={4}
+            intensity={3}
+            content="Acidification critique"
+          />
+          <HintItem
+            x={45}
+            y={28}
+            generation={4}
+            intensity={3}
+            content="Perturbation totale des cycles climatiques mondiaux"
+          />
+          <HintItem
+            x={48}
+            y={40}
+            generation={4}
+            intensity={3}
+            content="Effondrement des chaînes alimentaires marines"
+          />
+          <HintItem
+            x={45}
+            y={10}
+            generation={4}
+            intensity={3}
+            content="Désoxygénation accrue"
           />
         </div>
         <div className='right split'>
           <Image src="/personna.png" alt="une personne" width="141" height="388" />
           <HintItem
-            x={65}
+            x={35}
+            y={18}
+            generation={1}
+            content="Fatigue et faiblesse généralisées"
+          />
+          <HintItem
+            x={55}
+            y={25}
+            generation={1}
+            content="Déshydratation due à une sudation excessive"
+          />
+          <HintItem
+            x={49}
+            y={2}
+            generation={1}
+            content="Maux de tête et étourdissements"
+          />
+          <HintItem
+            x={42}
+            y={4}
+            generation={1}
+            content="Augmentation du rythme cardiaque"
+          />
+
+          <HintItem
+            x={49}
+            y={2}
+            generation={2}
+            intensity={2}
+            content="Confusion mentale et désorientation"
+          />
+          <HintItem
+            x={55}
+            y={25}
+            generation={2}
+            content="Crampes musculaires sévères"
+          />
+          <HintItem
+            x={48}
+            y={40}
+            generation={2}
+            content="Vomissements et nausées"
+          />
+          <HintItem
+            x={42}
+            y={4}
+            generation={2}
+            intensity={2}
+            content="Insuffisance rénale due à la déshydratation"
+          />
+
+          <HintItem
+            x={35}
+            y={18}
+            generation={3}
+            intensity={3}
+            content="Dommages aux organes internes"
+          />
+          <HintItem
+            x={45}
+            y={28}
+            generation={3}
+            intensity={2}
+            content="Évanouissements fréquents"
+          />
+          <HintItem
+            x={49}
+            y={2}
+            generation={3}
+            intensity={2}
+            content="Œdème cérébral (gonflement du cerveau)"
+          />
+          <HintItem
+            x={45}
             y={10}
-            content="maux de tête"
+            generation={3}
+            intensity={2}
+            content="Dysfonctionnement du système nerveux"
+          />
+
+          <HintItem
+            x={49}
+            y={2}
+            generation={4}
+            intensity={3}
+            content="Dommages irréversibles au cerveau"
+          />
+          <HintItem
+            x={45}
+            y={28}
+            generation={4}
+            intensity={3}
+            content="Arrêt cardiaque dû à un effort thermique"
+          />
+          <HintItem
+            x={48}
+            y={40}
+            generation={4}
+            intensity={3}
+            content="Syndrome de défaillance multiviscérale"
+          />
+          <HintItem
+            x={45}
+            y={10}
+            generation={4}
+            intensity={3}
+            content="Mort par coup de chaleur non traité"
           />
         </div>
     </div>
